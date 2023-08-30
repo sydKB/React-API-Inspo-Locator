86029ac5b4b679fcf4d75d4e8ea572675692235e
Hans <126028768+HansVanHousen@users.noreply.github.com>
2023-08-29 21:12:48 -0700

    added log out functionality

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../../utils/mutations';
+import React, { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
 
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -35,38 +35,68 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
+  const handleLogout = () => {
+    Auth.logout();
+  };
 
-    return (
-        <section>
-
-        <div className="login">
-            <h2>Log in</h2>
-            <form class="form login-form" onSubmit={handleFormSubmit}>
-                <div>
-                    <label for="email-login">email:</label>
-                    <input class="form-input" type="text" id="email-login"  name="email"  onChange={handleChange}/>
-                </div>
-                <div>
-                    <label for="password-login">password:</label>
-                    <input class="form-input" type="password" id="password-login" name="password" onChange={handleChange}/>
-                </div>
-                <div>
-                    <button type="submit">Log In</button>
-                    <Link to="/signup"> 
-                    <button class="profile-btn" type="submit">Sign Up</button>
+  return (
+    <section>
+      <div className="login">
+        <h2>Log in</h2>
+        <form class="form login-form" onSubmit={handleFormSubmit}>
+          <div>
+            <label for="email-login">email:</label>
+            <input
+              class="form-input"
+              type="text"
+              id="email-login"
+              name="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label for="password-login">password:</label>
+            <input
+              class="form-input"
+              type="password"
+              id="password-login"
+              name="password"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <Link>
+              {Auth.loggedIn() ? (
+                 <button
+                 type="submit"
+                 class="profile-btn"
+                 onClick={handleLogout}
+               >
+                 Logout
+               </button>
+                
+              ) 
+              : (
+                <Link to="/signup">
+                  <button type="submit">Log In</button>,
+                  <button class="profile-btn" type="submit">
+                  Sign Up
+                </button>
                 </Link>
-                    <p class="log-again"> </p>
-                </div>
-            </form>
-        </div>
-        <Outlet />
-        </section>
-    );   
-}
-
+              )
+              }
+            </Link>
+            <p class="log-again"> </p>
+          </div>
+        </form>
+      </div>
+      <Outlet />
+    </section>
+  );
+};
 
 export default Login;
